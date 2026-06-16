addf8abe18065bf8ff7ee492bd3d663da5a00ece
itsRev1L <itsmoody.ldbl@gmail.com>
2026-06-16 20:50:41 +0330

    a basic navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='fixed z-20 w-full   bg-red-500 mx-auto  flex items-center justify-between px-6  py-2 shadow-md'>
+      <div className='flex items-center '>
+        <img src="/Logo.png" alt="Logo" className='h-auto w-10' />
+        <span className='px-4 text-2xl font-bold text-white'>
+          Cubic Moves
+        </span>
+      </div>
+
+      <div>
+        <ul>
+          <li className='flex text-white text-base font-medium gap-6  '>
+            <ul className=' relative cursor-pointer after:left-0 after:bottom-1 after:h-px after:w-0 after:bg-white after:transition-all hover:after:w-full'>
+              Home
+            </ul>
+            <ul className=' relative cursor-pointer after:left-0 after:bottom-1 after:h-px after:w-0 after:bg-white after:transition-all hover:after:w-full'>
+              Games
+            </ul>
+            <ul className=' relative cursor-pointer after:left-0 after:bottom-1 after:h-px after:w-0 after:bg-white after:transition-all hover:after:w-full'>
+              About us
+            </ul>
+            <ul className=' relative cursor-pointer after:left-0 after:bottom-1 after:h-px after:w-0 after:bg-white after:transition-all hover:after:w-full'>
+              Shop
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
